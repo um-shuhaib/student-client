c206aacdae325bd2ecc698c5e0eca99f8b4f0354
um-shuhaib <shuhaib5560@gmail.com>
2025-12-16 12:18:20 +0530

    edit data fetched in the form of edit page

--- src/componets/EditStudent.jsx
+++ src/componets/EditStudent.jsx
@@ -7,11 +7,15 @@ import { useEffect } from 'react';
 
 function EditStudent() {
 
+    const [student,setStudent]=useState({
+        name:"",place:"",email:"",phone:""
+    })
     const {id}=useParams()
 
     useEffect(()=>{
         getStudent(id).then((res)=>{
             console.log(res.data);
+            setStudent(res.data)
             
             
 
@@ -24,16 +28,16 @@ function EditStudent() {
     <div className='vh-100 d-flex justify-content-center align-items-center'>
             <div className='w-75'>
                 <FloatingLabel controlId="floatingName" label="Name" className='mt-2'>
-                    <Form.Control type="text" placeholder="..." />
+                    <Form.Control type="text" placeholder="..." value={student.name} />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPlace" label="Place" className='mt-2'>
-                    <Form.Control type="text" placeholder="..." />
+                    <Form.Control type="text" placeholder="..." value={student.place} />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingEmail" label="Email" className='mt-2'>
-                    <Form.Control type="email" placeholder="..." />
+                    <Form.Control type="email" placeholder="..." value={student.email} />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPhone" label="Phone" className='mt-2'>
-                    <Form.Control type="text" placeholder="..." />
+                    <Form.Control type="text" placeholder="..." value={student.phone} />
                 </FloatingLabel>
             <div className='d-flex justify-content-center'>
                 <button className='btn btn-warning mt-2'>Add Student</button>
