0e566791e06869bf22955f00f6f6cdacb7201b5d
um-shuhaib <shuhaib5560@gmail.com>
2025-12-16 11:52:38 +0530

    added student and uses usenavigate

--- src/componets/AddStudent.jsx
+++ src/componets/AddStudent.jsx
@@ -2,30 +2,51 @@ import React from 'react'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
+import {toast} from 'react-toastify'
+import { addStudent } from '../api/fetchApi';
+import {useNavigate} from 'react-router-dom'
 
 function AddStudent() {
+
+    const navigate=useNavigate()
 
     const [student,setStudent]=useState({
         name:"",place:"",email:"",phone:""
     })
+    // console.log(student)
+    const formSubmit=()=>{
+        const {name,place,email,phone} = student 
+        if (!name || !place || !email || !phone){
+            toast("invalid credentioal")
+        }else{
+            addStudent(student).then((res)=>{
+                console.log(res.data);
+                toast("data added")
+                navigate('/')
+                
+            })
+        }
+
+    }
+    
 
     return (
         <div className='vh-100 d-flex justify-content-center align-items-center'>
             <div className='w-75'>
-                <FloatingLabel controlId="floatingPassword" label="Name" className='mt-2'>
+                <FloatingLabel controlId="floatingName" label="Name" className='mt-2'>
                     <Form.Control type="text" placeholder="..." onChange={(e)=>{setStudent({...student,name:e.target.value})}} />
                 </FloatingLabel>
-                <FloatingLabel controlId="floatingPassword" label="Place" className='mt-2'>
+                <FloatingLabel controlId="floatingPlace" label="Place" className='mt-2'>
                     <Form.Control type="text" placeholder="..." onChange={(e)=>{setStudent({...student,place:e.target.value})}} />
                 </FloatingLabel>
-                <FloatingLabel controlId="floatingPassword" label="Email" className='mt-2'>
+                <FloatingLabel controlId="floatingEmail" label="Email" className='mt-2'>
                     <Form.Control type="email" placeholder="..." onChange={(e)=>{setStudent({...student,email:e.target.value})}} />
                 </FloatingLabel>
-                <FloatingLabel controlId="floatingPassword" label="Phone" className='mt-2'>
+                <FloatingLabel controlId="floatingPhone" label="Phone" className='mt-2'>
                     <Form.Control type="text" placeholder="..." onChange={(e)=>{setStudent({...student,phone:e.target.value})}} />
                 </FloatingLabel>
             <div className='d-flex justify-content-center'>
-                <button className='btn btn-warning mt-2'>Add Student</button>
+                <button className='btn btn-warning mt-2' onClick={()=>{formSubmit()}} >Add Student</button>
             </div>
             </div>
         </div>
